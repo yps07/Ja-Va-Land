df989a6b47e90eff7183575135c62749b547b0df
muffadal14 <162454004+muffadal14@users.noreply.github.com>
2024-11-29 19:03:28 +0530

    Update Glass.java

--- JaVaLand/Glass.java
+++ JaVaLand/Glass.java
@@ -1,10 +1,7 @@
 package io.github.JaVaLand;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-
 public class Glass extends Block{
-    public Glass(Core game, String s){
-        super(game, 40, new Texture(Gdx.files.internal(s)));
+    public Glass(String s){
+        super(40, s);
     }
 }
