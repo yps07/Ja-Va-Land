9db758fbab69e1fb63151386c95a666ea1de8e7c
yps <162453910+yps07@users.noreply.github.com>
2024-11-29 19:06:51 +0530

    Update Wood.java

--- JaVaLand/Wood.java
+++ JaVaLand/Wood.java
@@ -1,10 +1,11 @@
 package io.github.JaVaLand;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-
 public class Wood extends Block{
-    public Wood(Core game, String s){
-        super(game, 30, new Texture(Gdx.files.internal(s)));
+    public Wood(String s){
+        super(30, s);
+    }
+
+    public Wood(){
+        super(30);
     }
 }
