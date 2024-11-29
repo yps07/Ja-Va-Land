ddeace26fd031bf876dc69421a3894025e95a318
yps <162453910+yps07@users.noreply.github.com>
2024-11-29 19:04:47 +0530

    Update Red.java

--- JaVaLand/Red.java
+++ JaVaLand/Red.java
@@ -1,11 +1,8 @@
 package io.github.JaVaLand;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-
 public class Red extends Bird{
-    public Red(Core game){
-        super(game, 5, 50, new Texture(Gdx.files.internal("red.png")));
-        super.setSize(45, 45);
+    public Red(){
+        super(3F, 75, "red.png");
+        super.setSize(50, 50);
     }
 }
