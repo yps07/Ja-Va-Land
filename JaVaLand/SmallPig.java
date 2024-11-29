4e565c96b179c239d4b924f909bf74e95ec0941f
yps <162453910+yps07@users.noreply.github.com>
2024-11-29 19:05:08 +0530

    Update SmallPig.java

--- JaVaLand/SmallPig.java
+++ JaVaLand/SmallPig.java
@@ -1,10 +1,11 @@
 package io.github.JaVaLand;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-
 public class SmallPig extends Pig {
-    public SmallPig(Core game){
-        super(game, 40, 40, 50, new Texture(Gdx.files.internal("s_pig.png")));
+    public SmallPig(){
+        super(40, 40, 50, "s_pig.png");
+    }
+
+    public SmallPig(boolean for_test){
+        super(40, 40, 50);
     }
 }
