5acee61322e21ad798f0c9326903c9fe47d2d439
yps <162453910+yps07@users.noreply.github.com>
2024-11-29 19:11:12 +0530

    Add files via upload

---
+++ JaVaLand/MsgScreen.java
@@ -0,0 +1,137 @@
+package io.github.JaVaLand;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class MsgScreen implements Screen {
+
+    private Texture img;
+    private Sprite sprite;
+    private final Core game;
+    private BitmapFont font, font1;
+    private Skin skin, skin1;
+    private Stage stage;
+
+    private Music bgm;
+    private Sound click_sound, bird_sound;
+
+    public MsgScreen(Core game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        bgm = Gdx.audio.newMusic(Gdx.files.internal("audio/AB_space_level_menu.mp3"));
+        click_sound = Gdx.audio.newSound(Gdx.files.internal("audio/Sound Effects - ButtonProceed.mp3"));
+
+        bgm.setLooping(true);
+        bgm.play();
+
+        img = new Texture("bg.jpg");
+        sprite = new Sprite(img);
+        sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        sprite.setPosition(0, 0);
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("abfont1.ttf")); // Replace with your TTF file
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 40;
+        font = generator.generateFont(parameter);
+
+        FreeTypeFontParameter parameter1 = new FreeTypeFontParameter();
+        parameter1.size = 60;
+        font1 = generator.generateFont(parameter1);
+        generator.dispose();
+
+        skin = new Skin();
+        skin.add("default", font);
+
+        skin1 = new Skin();
+        skin1.add("default", font1);
+
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = font;
+        textButtonStyle.fontColor = Color.WHITE;
+        textButtonStyle.downFontColor = new Color(0.5f, 0, 0, 1);
+
+        TextButton back = new TextButton("Back", textButtonStyle);
+        back.setPosition(625, 210);
+
+        stage.addActor(back);
+        Gdx.input.setInputProcessor(stage);
+
+        back.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new Menu(game));
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.batch.begin();
+
+        sprite.draw(game.batch);
+
+        font1.draw(game.batch, "No saved Games!", 315, 350);
+
+        game.batch.end();
+
+        stage.act(delta);
+        stage.draw();
+
+        if(Gdx.input.justTouched()){
+            click_sound.play(0.9f);
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {
+        bgm.stop();
+        bgm.dispose();
+        click_sound.dispose();
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+        font.dispose();
+        skin.dispose();
+        stage.dispose();
+        bgm.stop();
+        bgm.dispose();
+        click_sound.dispose();
+    }
+}
